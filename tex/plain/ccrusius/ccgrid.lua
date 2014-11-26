ddfca45b84cff679d59ba9185c4d13399ab31f1b
Cesar Crusius <ccrusius@cisco.com>
2014-11-26 08:19:26 -0800

    ccgrid log levels

--- tex/plain/ccrusius/ccgrid.lua
+++ tex/plain/ccrusius/ccgrid.lua
@@ -13,6 +13,28 @@
 local baselineskip = 0
 local haccum = 0
 local lastprebox = nil
+
+--- The ccgrid log level.
+-- Defines the amout of verbiage we are expected to produce. The values are as
+-- follows:
+--
+--     0. nothing
+--     1. adds a baseline grid to every page
+--     2. prints the contents of \box255 at every output
+--     3. traces page building process
+local loglevel = 0
+
+--- Set the log level.
+-- @param x The new log level.
+local function setloglevel(x) loglevel = x end
+
+--- Type out a message to the console and TeX log if the log level is high enough.
+-- @param lvl The minimum log level required for the message to be issued.
+-- @param str The message to be issued.
+local function typeout(lvl,str)
+  if loglevel >= lvl then texio.write_nl(str) end
+end
+
 --------------------------------------------------------------------------------
 --
 -- Basic functions
@@ -55,10 +77,10 @@ local function buildpage_hlist(head,cur)
   --
   local skip = baselineskip*math.ceil(haccum/baselineskip) - haccum
   if skip > 0 then
-    texio.write_nl(string.format("  ( Shifting %fpt )",skip/2^16))
+    typeout(3,string.format("  ( shifting %fpt )",skip/2^16))
     head = node.insert_before(head,cur,ccbase.mkglue(skip,0,0,0,0))
   else
-    texio.write_nl("  ( nop )")
+    typeout(3,"  ( nop )")
   end
   haccum = haccum + skip
   haccum = haccum + cur.depth
@@ -79,42 +101,47 @@ local function buildpage_box(head)
     head = lastprebox.next
     prebox = nil
   end
-  ccshowbox.showheadlist(head,0,"  << .")
+  if loglevel>= 3 then ccshowbox.showheadlist(head,0,"  << .") end
   local cur = head
   while cur do
     local fn = buildpage_nodes[cur.id]
     if fn then head, cur = fn(head,cur) end
     cur = cur.next
   end
-  ccshowbox.showheadlist(head,0,"  >> .")
+  if loglevel >= 3 then ccshowbox.showheadlist(head,0,"  >> .") end
+end
+
+
+local function buildpage_nop(head)
+  if loglevel >= 3 then
+    ccshowbox.showheadlist(head,0,"  == .")
+  end
 end
 
 local buildpage_actions = {
   after_display  = buildpage_box,
   after_output   = buildpage_after_output,
-  before_display = function(head) ccshowbox.showheadlist(head,0,"  == .") end,
+  before_display = buildpage_nop,
   box            = buildpage_box,
-  hmode_par      = function(head) ccshowbox.showheadlist(head,0,"  == .") end,
-  new_graf       = function(head) ccshowbox.showheadlist(head,0,"  == .") end,
+  hmode_par      = buildpage_nop,
+  new_graf       = buildpage_nop,
   --
   -- pre_box: Signals that contrib_head contains material that is already
   -- present and does not need to be re-processed.
   --
   pre_box        = function(head) lastprebox=node.slide(head) end,
-  vmode_par      = function(head) ccshowbox.showheadlist(head,0,"  == .") end,
+  vmode_par      = buildpage_nop,
 }
 
 local function buildpage(info)
   local head = tex.lists.contrib_head
-  texio.write_nl(string.format("BUILDPAGE("..info..") haccum=%fpt",haccum/2^16))
+  typeout(3,string.format("BUILDPAGE("..info..") haccum=%fpt",haccum/2^16))
 
   local action = buildpage_actions[info]
   if action then action(head) end
 end
 
 local function output(head)
-  texio.write_nl("PRE_OUTPUT_BOX:")
-  ccshowbox.showheadlist(head,0,".")
   --
   -- Insert a "raggedbottom-Xtreme" glue at the end of the page.
   --
@@ -123,6 +150,11 @@ local function output(head)
   -- page, so we insert 10,000 of them. No stretch in our pages!
   --
   node.insert_after(head,node.slide(head),ccbase.mkglue(0,10000*2^16,3,0,0))
+  --
+  -- Log
+  --
+  typeout(2,"OUTPUT BOX:")
+  if loglevel >= 2 then ccshowbox.showheadlist(head,0,".") end
   --
   -- Done
   --
@@ -136,4 +168,5 @@ return {
   snapdown = snapdown,
   setgrid = setgrid,
   freeze = freeze,
+  setloglevel = setloglevel,
 }
