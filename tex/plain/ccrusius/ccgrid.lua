651349d3a77adbe41f54d8dc0b7b23316d36696e
Cesar Crusius <cesar.crusius@stanfordalumni.org>
2014-12-05 20:21:02 -0800

    all ccgrid files generated by ccweb

--- tex/plain/ccrusius/ccgrid.lua
+++ tex/plain/ccrusius/ccgrid.lua
@@ -1,60 +1,58 @@
---------------------------------------------------------------------------------
---
--- Algorithm variables
---
--- haccum: When incrementally processing a vlist, we keep track of how
---     much vertical material was added so far in this variable.
--- lastprebox: LuaTeX, when breaking paragraphs into lines, signals that
---     new material will be added with a pre_box buildpage_filter call.
---     We save the value so we don't re-process things.
---
+local exports = {}
+local loglevel = 0
+local baselineskip = 0
 local haccum = 0
 local lastprebox = nil
-
-local function buildpage_glue(head,cur)
-  local spec = cur.spec
+local function setloglevel(x) loglevel = x end
+exports["setloglevel"] = setloglevel
+local function typeout(lvl,str)
+  if loglevel >= lvl then texio.write_nl(str) end
+end
+local function setgrid(x) baselineskip=x end
+exports["setgrid"] = setgrid
+local function snapdown(x)
+  return baselineskip*math.floor(x/baselineskip)
+end
+exports["snapdown"] = snapdown
+local function freeze(x)
+  return (ccbase.spstr(ccbase.tosp(x))).." plus 0pt minus 0pt"
+end
+exports["freeze"] = freeze
+local function output(head)
+  node.insert_after(head,node.slide(head),ccbase.mkglue(0,10000*2^16,3,0,0))
+  typeout(2,"OUTPUT BOX:")
+  if loglevel >= 2 then ccshowbox.showheadlist(head,0,".") end
+  return true
+end
+callback.register("pre_output_filter",output)
+local function freeze_glue(head,glue)
+  local spec = glue.spec
   local width = spec.width
   haccum = haccum + width
-  if spec.stretch == 0 and spec.shrink == 0 then return head, cur end
+  if spec.stretch == 0 and spec.shrink == 0 then return head, glue end
 
-  local noglue = ccbase.mkglue(cur.spec.width,0,0,0,0)
-  head, noglue = node.insert_after(head,cur,noglue)
-  head, noglue = node.remove(head,cur)
-  --
-  -- I think we should free "cur" now, but that makes things crash.
-  --
-  -- if spec.writable then node.free(cur) end
+  local noglue = ccbase.mkglue(glue.spec.width,0,0,0,0)
+  head, noglue = node.insert_after(head,glue,noglue)
+  head, noglue = node.remove(head,glue)
+
+  -- I think we should free "glue" now, but that makes things crash.
+  -- if spec.writable then node.free(glue) end
   return head, noglue
 end
-
-local function buildpage_hlist(head,cur)
-  haccum = haccum + cur.height
-  --
-  -- If necessary, insert enough glue to make the baseline of the hbox aling
-  -- with the baseline grid.
-  --
+local function align_hlist(head,hlist)
+  haccum = haccum + hlist.height
   local skip = baselineskip*math.ceil(haccum/baselineskip) - haccum
   if skip > 0 then
     typeout(3,string.format("  ( shifting %fpt )",skip/2^16))
-    head = node.insert_before(head,cur,ccbase.mkglue(skip,0,0,0,0))
+    head = node.insert_before(head,hlist,ccbase.mkglue(skip,0,0,0,0))
+    haccum = haccum + skip
   else
     typeout(3,"  ( nop )")
   end
-  haccum = haccum + skip
-  haccum = haccum + cur.depth
-  return head, cur
+  haccum = haccum + hlist.depth
+  return head, hlist
 end
-
-local buildpage_nodes = {
-  [ccbase.GLUE_TYPE]  = buildpage_glue,
-  [ccbase.HLIST_TYPE] = buildpage_hlist,
-}
-
-local function buildpage_after_output(head)
-  haccum = 0
-end
-
-local function buildpage_box(head)
+local function align_box(head)
   if lastprebox then
     head = lastprebox.next
     prebox = nil
@@ -62,62 +60,48 @@ local function buildpage_box(head)
   if loglevel>= 3 then ccshowbox.showheadlist(head,0,"  << .") end
   local cur = head
   while cur do
-    local fn = buildpage_nodes[cur.id]
+    local fn = {
+        [ccbase.GLUE_TYPE]  = freeze_glue,
+        [ccbase.HLIST_TYPE] = align_hlist,
+      }[cur.id]
     if fn then head, cur = fn(head,cur) end
     cur = cur.next
   end
   if loglevel >= 3 then ccshowbox.showheadlist(head,0,"  >> .") end
 end
-
-
-local function buildpage_nop(head)
-  if loglevel >= 3 then
-    ccshowbox.showheadlist(head,0,"  == .")
-  end
-end
-
 local buildpage_actions = {
-  after_display  = buildpage_box,
-  after_output   = buildpage_after_output,
-  before_display = buildpage_nop,
-  box            = buildpage_box,
-  hmode_par      = buildpage_nop,
-  new_graf       = buildpage_nop,
-  --
-  -- pre_box: Signals that contrib_head contains material that is already
-  -- present and does not need to be re-processed.
-  --
-  pre_box        = function(head) lastprebox=node.slide(head) end,
-  vmode_par      = buildpage_nop,
+  after_output = function(head) haccum=0 end,
+  before_display = function(head)
+                     if loglevel >= 3 then
+                       ccshowbox.showheadlist(head,0,"  == .")
+                     end
+                   end,
+  hmode_par      = function(head)
+                     if loglevel >= 3 then
+                       ccshowbox.showheadlist(head,0,"  == .")
+                     end
+                   end,
+  new_graf       = function(head)
+                     if loglevel >= 3 then
+                       ccshowbox.showheadlist(head,0,"  == .")
+                     end
+                   end,
+  vmode_par      = function(head)
+                     if loglevel >= 3 then
+                       ccshowbox.showheadlist(head,0,"  == .")
+                     end
+                   end,
+  pre_box = function(head) lastprebox=node.slide(head) end,
+  after_display  = align_box,
+  box            = align_box,
+  
+  
 }
-
-local function buildpage(info)
+local function buildpage(reason)
   local head = tex.lists.contrib_head
-  typeout(3,string.format("BUILDPAGE("..info..") haccum=%fpt",haccum/2^16))
-
-  local action = buildpage_actions[info]
+  typeout(3,string.format("BUILDPAGE("..reason..") haccum=%fpt",haccum/2^16))
+  local action = buildpage_actions[reason]
   if action then action(head) end
 end
-
-local function output(head)
-  --
-  -- Insert a "raggedbottom-Xtreme" glue at the end of the page.
-  --
-  -- In the original definition, raggedbottom inserts a vfil at the end of the
-  -- page. But a single vfil can't nullify other vfils that may be in the
-  -- page, so we insert 10,000 of them. No stretch in our pages!
-  --
-  node.insert_after(head,node.slide(head),ccbase.mkglue(0,10000*2^16,3,0,0))
-  --
-  -- Log
-  --
-  typeout(2,"OUTPUT BOX:")
-  if loglevel >= 2 then ccshowbox.showheadlist(head,0,".") end
-  --
-  -- Done
-  --
-  return true
-end
-
 callback.register("buildpage_filter",buildpage)
-callback.register("pre_output_filter",output)
+return exports
